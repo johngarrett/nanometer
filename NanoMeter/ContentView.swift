d446fb465fe4ced2b90fa0169391550ba323bff8
John Garrett <jacgarrett18@gmail.com>
2020-10-08 15:42:57 -0400

    inital commit, layout structure

--- NanoMeter/ContentView.swift
+++ NanoMeter/ContentView.swift
@@ -1,16 +1,81 @@
-//
-//  ContentView.swift
-//  NanoMeter
-//
-//  Created by garrepi on 10/8/20.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var model = ContentViewModel()
+    @State var isRecoding = false
+    
+    private var mph = { String(format: "%.2f", $0 * 2.37) }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Current Speed")
+                .font(.body)
+                .fontWeight(.medium)
+            HStack {
+                Spacer()
+                VStack(alignment: .center) {
+                    Text("x")
+                        .font(.body)
+                    Image(systemName: "arrow.right")
+                    HStack {
+                        Text(mph(model.currentSpeed?.0 ?? 0))
+                        Text("MPH")
+                            .font(.caption)
+                            .fontWeight(.medium)
+                            .foregroundColor(.gray)
+                    }.frame(maxWidth: .infinity)
+                }
+                Spacer()
+                VStack(alignment: .center) {
+                    Text("y")
+                        .font(.body)
+                    Image(systemName: "arrow.up")
+                    HStack {
+                        Text(mph(model.currentSpeed?.1 ?? 0))
+                        Text("MPH")
+                            .font(.caption)
+                            .fontWeight(.medium)
+                            .foregroundColor(.gray)
+                    }.frame(maxWidth: .infinity)
+                }
+                Spacer()
+                VStack(alignment: .center) {
+                    Text("z")
+                        .font(.body)
+                    Image(systemName: "arrow.uturn.up")
+                    HStack {
+                        Text(mph(model.currentSpeed?.2 ?? 0))
+                        Text("MPH")
+                            .font(.caption)
+                            .fontWeight(.medium)
+                            .foregroundColor(.gray)
+                    }.frame(maxWidth: .infinity)
+                }
+                Spacer()
+            }.padding()
+                
+            Text("Current Acceleration")
+                .font(.body)
+                .fontWeight(.medium)
+            VStack(spacing: 4) {
+                Text("x:" + mph(model.acceleration?.x ?? 0))
+                Text("y:" + mph(model.acceleration?.y ?? 0))
+                Text("z:" + mph(model.acceleration?.z ?? 0))
+            }.font(.caption)
+            
+            Spacer()
+            Button {
+                print("action!")
+            } label: {
+                HStack(spacing: 8) {
+                    Image(systemName: "play.circle")
+                    Text("Start Recording")
+                }.padding()
+            }
+            .foregroundColor(.white)
+            .background(Color.red)
+            .cornerRadius(16)
+        }
     }
 }
 
---
+++ NanoMeter/ContentViewModel.swift
@@ -0,0 +1,38 @@
+import Combine
+import SwiftUI
+import CoreMotion
+
+class ContentViewModel: ObservableObject {
+    @Published var currentSpeed: (Double, Double, Double)?
+    @Published var acceleration: CMAcceleration?
+    private var previousAcceleration: CMAcceleration?
+    let motionManager = CMMotionManager()
+    var timer: Timer!
+    
+    init() {
+        motionManager.accelerometerUpdateInterval = 1/60
+        motionManager.startAccelerometerUpdates()
+
+        timer = Timer.scheduledTimer(timeInterval: 1/10, target: self, selector: #selector(ContentViewModel.update), userInfo: nil, repeats: true)
+    }
+    
+    @objc func update() {
+        if let accelerometerData = motionManager.accelerometerData {
+            previousAcceleration = acceleration
+            acceleration = accelerometerData.acceleration
+            updateSpeed()
+        } else {
+            print("no acceleration data")
+        }
+    }
+    
+    private func updateSpeed() {
+        guard let acceleration = acceleration, let prevAcceleration = previousAcceleration else {
+            return
+        }
+        let x = (acceleration.x - prevAcceleration.x) / (1/10) / 0.98
+        let y = (acceleration.y - prevAcceleration.y) / (1/10) / 0.98
+        let z = (acceleration.z - prevAcceleration.z) / (1/10) / 0.98
+        currentSpeed = (x, y, z)
+    }
+}
