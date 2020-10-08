41b756b0f1deee62614c41cbfd4a8cb2d3d09109
John Garrett <jacgarrett18@gmail.com>
2020-10-08 14:04:14 -0400

    Initial Commit

---
+++ NanoMeter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NanoMeter
+//
+//  Created by garrepi on 10/8/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
